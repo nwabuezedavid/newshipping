fa84d068bdf8b7fa16ec505ec27131235afb1ac2
NwabuezeDavid <davidnwabueze333@gmail.com>
2024-10-21 14:29:47 +0100

    Your commit message

--- shipping/settings.py
+++ shipping/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-$tx+@ndz+su!0qu+511o61e*@adhg%5airgw-p1ex_kzk8%-md
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
