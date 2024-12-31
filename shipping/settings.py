dbbd9fe2d45950b972317a5938403627b4560e1e
NwabuezeDavid <davidnwabueze333@gmail.com>
2024-12-31 09:39:09 +0100

    first commit

--- shipping/settings.py
+++ shipping/settings.py
@@ -79,14 +79,23 @@ WSGI_APPLICATION = 'shipping.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'verceldb',
+        'USER': 'default',
+        'PASSWORD': 'wK7Pi3BHmpNd',
+        'HOST': 'ep-divine-cloud-a2guvjw4-pooler.eu-central-1.aws.neon.tech',
+        'PORT': '5432',
     }
 }
-
-
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
 
